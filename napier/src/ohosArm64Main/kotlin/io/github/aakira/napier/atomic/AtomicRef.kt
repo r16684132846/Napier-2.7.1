88c3752f4e6282767a6f77266d5c474aea8dfc46
REP <16684132846@163.com>
2025-10-24 19:21:48 +0800

    配置服务器和本地发布环境

--- napier/src/ohosArm64Main/kotlin/io/github/aakira/napier/atomic/AtomicRef.kt
+++ napier/src/ohosArm64Main/kotlin/io/github/aakira/napier/atomic/AtomicRef.kt
@@ -1,3 +1,11 @@
 package io.github.aakira.napier.atomic
 
-internal actual class AtomicRef<T> actual constructor(actual var value: T)
+internal actual class AtomicRef<T> actual constructor(value: T) {
+    private var _value: T = value
+
+    actual var value: T
+        get() = _value
+        set(value) {
+            _value = value
+        }
+}
